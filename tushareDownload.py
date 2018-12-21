6eed50512dbf343e0043a88ee62a647a841008ba
mrzsh <453452586@qq.com>
2018-12-21 15:28:10 +0800

    缓存粒度细化到天

--- tushareDownload.py
+++ tushareDownload.py
@@ -38,18 +38,45 @@ def get_share_data(end_data = "2018-12-31", freq = 'D') :
     return df
 
 test = ""
-@lru_cache(maxsize=20)
 def _get_share_data(end_data = "2018-12-31", freq = 'D') :
     global test
+    daily_data = _get_index_data(end_data = end_data)
+    data = None
+    if freq == 'D':
+        data = daily_data
+    elif freq == 'W':
+        daily_data['index_date'] = pd.to_datetime(daily_data['trade_date'])
+        daily_data = daily_data.set_index(['index_date'])
+        rdf = daily_data.resample('W').agg({
+            'close': lambda r: r.tail(1),
+            'open': lambda r: r.head(1),
+            'high': 'max',
+            'low': 'min',
+            'trade_date': lambda r: r.tail(1),
+        })
+        rdf = rdf.dropna(axis=0)
+        rdf = rdf.reset_index(drop=False)
+        # rdf['trade_date'] = rdf['trade_date'].apply(lambda x: datetime.strftime(x,"%Y-%m-%d"))
+        data = rdf
+    test = end_data
+    return data
+
+@lru_cache(maxsize=20)
+def _get_index_data(from_data = '2005-01-01', end_data = '2018-12-31'):
     end_data = datetime.strptime(end_data, "%Y-%m-%d")
     start_date_term = datetime.strptime("2005-01-01", "%Y-%m-%d")
     end_date_term = datetime.strptime("2005-12-31", "%Y-%m-%d")
     pre_data = None
     while start_date_term < end_data:
-        df = general_data(ts_code='000300.sh',
-                        asset='I', freq= freq,
+        df = ts.pro_bar(pro_api=api,
+                        ts_code='000300.sh',
+                        asset= 'I',
+                        freq='D',
                         start_date=toString(start_date_term),
                         end_date=toString(end_date_term))
+        if df is None :
+            raise Exception('date from %s -> %s is null'% (start_date_term, end_date_term))
+        df = df[::][::-1]
         # df = df[['close', 'trade_date', 'open', 'high', 'low']][::-1]
         if pre_data is None:
             pre_data = df
@@ -58,9 +85,9 @@ def _get_share_data(end_data = "2018-12-31", freq = 'D') :
         pre_year = start_date_term.year
         start_date_term = start_date_term.replace(year=pre_year + 1)
         end_date_term = end_date_term.replace(year=pre_year + 1)
-    test = end_data
     return pre_data
 
+# 废弃
 def general_data(start_date, end_date, asset, freq, ts_code):
     df = ts.pro_bar(pro_api = api,
                     ts_code=ts_code,
